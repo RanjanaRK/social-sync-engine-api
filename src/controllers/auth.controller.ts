654ddaab6ba84ba27c8c470a5db572bc32229617
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-24 15:51:55 +0530

    project setup

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,12 @@
+export const registerController = async () => {
+  try {
+  } catch (error) {}
+};
+export const loginController = async () => {
+  try {
+  } catch (error) {}
+};
+export const logoutController = async () => {
+  try {
+  } catch (error) {}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
+import dotenv from "dotenv";
 import app from "./server";
+import { db } from "./config/db";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import express from "express";
+import helmet from "helmet";
+
+dotenv.config();
+db();
+app.set("trust proxy", true);
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
 
 app.listen(5000, () => {
   console.log("server is running okay");
