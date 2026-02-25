25087fd527bee91639faf4c4780fbead417f922c
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-25 22:12:20 +0530

    Create post.model.ts

---
+++ src/models/post.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    caption: {
+      type: String,
+      trim: true,
+      maxlength: [1000, "Caption cannot exceed 1000 characters"],
+      default: "",
+    },
+    postImage: {
+      type: String,
+      trim: true,
+      required: [true, "imgUrl is required for creating an post"],
+    },
+
+    likesCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    commentsCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+    visibility: {
+      type: String,
+      enum: ["public", "private", "friends"],
+      default: "public",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "user id is required for creating an post"],
+    },
+  },
+  { timestamps: true },
+);
+
+postSchema.index({ user: 1, createdAt: -1 });
+postSchema.index({ createdAt: -1 });
+
+export const postModel = mongoose.model("posts", postSchema);
