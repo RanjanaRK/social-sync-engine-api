bf5a74723eb71616a1a5ef113371304248bbee87
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-27 20:29:21 +0530

    file upload

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,8 +1,37 @@
+import ImageKit, { toFile } from "@imagekit/nodejs";
 import { Request, Response } from "express";
+import { posts } from "../models/post.model";
+
+const imagekit = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
 
 export const createPostController = async (req: Request, res: Response) => {
   try {
-    const token = req.cookies.token;
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Image required",
+      });
+    }
+
+    const uploaded = await imagekit.files.upload({
+      file: req.file.buffer,
+
+      fileName: Date.now() + "-" + req.file.originalname,
+    });
+    const post = await posts.create({
+      caption: req.body.caption || "",
+
+      imgUrl: uploaded.url,
+
+      user: req.user.id,
+    });
+
+    return res.status(201).json({
+      message: "Post created",
+
+      post,
+    });
   } catch (error) {
     console.error("Post Error:", error);
 
