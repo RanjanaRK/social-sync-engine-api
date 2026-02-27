3a2e34f077c6e69a57e45cdc69a528a8442afb13
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-27 14:33:15 +0530

    Update auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import { users } from "../models/user.model";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
+// REGISTRATION
 export const registerController = async (req: Request, res: Response) => {
   try {
     const { email, password, username } = req.body;
@@ -62,10 +63,74 @@ export const registerController = async (req: Request, res: Response) => {
   }
 };
 
-export const loginController = async () => {
+// LOGIN
+export const loginController = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { email, password, username } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const existingUser = await users
+      .findOne({
+        $or: [{ email }, { username }],
+      })
+      .select("+password");
+
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password,
+    );
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "password invalid",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        username: existingUser.username,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "1d",
+      },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        username: existingUser.username,
+        email: existingUser.email,
+        bio: existingUser.bio || "",
+        profileImage: existingUser.profileImage || "",
+      },
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
+
+// LOGOUT
 
 export const logoutController = async () => {
   try {
