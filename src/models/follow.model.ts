bd676c5fea4b5bc7af156435a86a24d8e48ae1f4
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-22 16:44:18 +0530

    model

---
+++ src/models/follow.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: String,
+    },
+    followee: {
+      type: String,
+    },
+    status: {
+      type: String,
+      default: "pending",
+      enum: {
+        values: ["pending", "accepted", "rejected"],
+        message: "status can only be pending, accepted or rejected",
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+followSchema.index({ follower: 1, followee: 1 }, { unique: true });
+
+export const followModel = mongoose.model("follows", followSchema);
