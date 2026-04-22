477b7ddae789999b2a250fe5130b435ecd31957f
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-22 08:55:17 +0530

    Update post.routes.ts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
-import multer, { memoryStorage } from "multer";
-import { identifyUser } from "../middlewares/auth.middleware.js";
 import { createPostController } from "../controllers/post.controller.js";
+import { identifyUser } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 export const postRouter = Router();
