66ae38022842caf078de1bb691491ea87e2bcd01
RanjanaRK <ranjanakumari.me@gmail.com>
2026-04-27 23:41:46 +0530

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -22,3 +22,55 @@ export const getAllUsersController = async (req: Request, res: Response) => {
     return res.status(500).json({ success: false, message: "Server error" });
   }
 };
+
+export const updateUserController = async (req: Request, res: Response) => {
+  try {
+    const userId = req.user?.id;
+
+    const { username, bio, profileImage } = req.body;
+
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const user = await users.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const updatedUser = await users.findByIdAndUpdate(
+      userId,
+      {
+        username,
+        bio,
+        profileImage,
+      },
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
