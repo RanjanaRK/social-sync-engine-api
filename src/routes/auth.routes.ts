173b98820581329fc8d62154a850735adef2361a
RanjanaRK <ranjanakumari.me@gmail.com>
2026-06-09 23:34:07 +0530

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,9 +4,12 @@ import {
   logoutController,
   registerController,
 } from "../controllers/auth.controller.js";
+import { identifyUser } from "../middlewares/auth.middleware.js";
+import { getCurrentUserProfileController } from "../controllers/user.controller.js";
 
 export const authRouter = Router();
 
 authRouter.post("/register", registerController);
 authRouter.post("/login", loginController);
 authRouter.post("/logout", logoutController);
+authRouter.get("/me", identifyUser, getCurrentUserProfileController);
